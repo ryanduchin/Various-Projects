1942bf611fc74d9e730198ebb2f23954eb758abf
Ryan Duchin <ryanduchin@gmail.com>
2015-06-24 12:12:16 -0700

    .

--- require.rb
+++
@@ -1,8 +0,0 @@
-require 'singleton'
-require 'sqlite3'
-require 'byebug'
-require_relative 'reply'
-require_relative 'QuestionLike'
-require_relative 'QuestionFollow'
-require_relative 'Question'
-require_relative 'User'
