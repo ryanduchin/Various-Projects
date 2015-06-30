d9c8d894222adeedfea8857bcd1a853763a893e3
Ryan Duchin <ryanduchin@gmail.com>
2015-06-30 15:56:22 -0700

    fixed rails projects folder

--- Rails-Projects/w4d4_musicapp/MusicApp/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-  resources :users, only: [:new, :create, :show]
-  resource :session, only: [:new, :create, :destroy]
-end
--- Rails-Projects/w4d4_musicapp/MusicApp/db/migrate/20150514204008_create_music_tables.rb
+++
@@ -1,6 +0,0 @@
-class CreateMusicTables < ActiveRecord::Migration
-  def change
-    create_table :music_tables do |t|
-    end
-  end
-end
